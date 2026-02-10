8855a60025a2d8cdc7b38e193b7b5d12c2c0c653
Sajid Mehmood <147178031+thesaajii@users.noreply.github.com>
2026-02-10 16:55:06 +0500

    Delete 03-LSB-Coding directory

--- 03-LSB-Coding/data_extracting.m
+++
@@ -1,9 +0,0 @@
-close all; clear all; clc;
-
-[FileName,PathName] = uigetfile({'.wav'}, 'Select stego audio:');
-wavin = [PathName FileName];
-
-password = 'mypassword123';
-
-msg = lsb_dec(wavin, password);
-fprintf('Retrieved message: %s\n', msg);
